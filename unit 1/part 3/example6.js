58b6cf3cbb41a84ac8fb6bcdd4265518cb9f0223
Arturo Bernal <arturo@iessanvicente.com>
2017-09-29 16:15:06 +0200

    29-9-2017

--- unit 1/part 3/example6.js
+++ unit 1/part 3/example6.js
@@ -1,8 +1,66 @@
 "use strict";
 
+const SERVER = 'http://arturober.com/productos';
+let products = [];
+
+function replaceProducts() {
+    let container = document.getElementById("productContainer");
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    products.forEach(p => {
+        appendProduct(p, container);
+    });
+}
+
+function appendProduct(p, container) {
+    let tr = document.createElement("tr");
+
+    let tdName = document.createElement("td");
+    tdName.innerText = p.name;
+
+    let tdDesc = document.createElement("td");
+    tdDesc.innerText = p.description;
+
+    let tdPhoto = document.createElement("td");
+
+    let img = document.createElement("img");
+    img.src = `${SERVER}/img/${p.photo}`;
+    tdPhoto.appendChild(img);
+
+    let tdDelete = document.createElement("td");
+    let buttonDelete = document.createElement("button");
+    buttonDelete.innerText = "Delete";
+    tdDelete.appendChild(buttonDelete);
+
+    buttonDelete.addEventListener('click', event => {
+        deleteProduct(p.id);
+    });
+
+    tr.appendChild(tdPhoto);
+    tr.appendChild(tdName)
+    tr.appendChild(tdDesc);
+    tr.appendChild(tdDelete);
+    tr.id = `p${p.id}`;
+    container.appendChild(tr);
+}
+
+function deleteProduct(id) {
+    var http = new XMLHttpRequest();
+    http.open('DELETE', `${SERVER}/product/${id}`, true);
+    http.send();
+
+    http.addEventListener('load', (event) => {
+        if (http.status === 200) {
+            let tr = document.getElementById(`p${id}`);
+            tr.parentElement.removeChild(tr);
+        }
+    });
+}
+
 function getProducts() {
     var http = new XMLHttpRequest();
-    http.open('GET', 'http://arturober.com/productos/product', true);
+    http.open('GET', `${SERVER}/product`, true);
     http.send();
     // http.timeout = 2;
 
@@ -18,21 +76,22 @@ function getProducts() {
 
     http.addEventListener('load', (event) => {
         if (http.status === 200) {
-            console.log(JSON.parse(http.responseText));
+            products = JSON.parse(http.responseText).products;
+            replaceProducts();
         }
     });
 }
 
 function addProduct(prodJSON) {
     var http = new XMLHttpRequest();
-    http.open('POST', 'http://arturober.com/productos/product/json', true);
-    http.setRequestHeader("Content-type", "application/json");  
+    http.open('POST', `${SERVER}/product/json`, true);
+    http.setRequestHeader("Content-type", "application/json");
     http.send(JSON.stringify(prodJSON));
 
     http.addEventListener('load', (event) => {
         if (http.status === 200) {
-            console.log(JSON.parse(http.responseText));
-            getProducts();
+            let p = JSON.parse(http.responseText).product;
+            appendProduct(p, document.getElementById("productContainer"));
         }
     });
 }
@@ -64,6 +123,11 @@ window.addEventListener("load", event => {
         reader.addEventListener("load", () => { //Converted into Base64 event (async)
             document.getElementById("preview").src = reader.result;
         }, false);
+    });
+
+    document.getElementById("orderName").addEventListener('click', e => {
+        products.sort((p1,p2) => p1.name.localeCompare(p2.name));
+        replaceProducts();
     });
 
 });
